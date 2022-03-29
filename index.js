8ee2179144421620a69f10db9d13ef42099af882
bernadette <aguirre.bernadette@outlook.com>
2022-03-29 14:17:57 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // code your solution here
+function superbowlWin(record){
+    function isWinningYear(year){
+        if(year.result === "W"){
+        return true
+    } else {
+        return false
+    }
+}
+
+const winningYearObject = record.find(isWinningYear) 
+
+if (winningYearObject){
+    return winningYearObject.year
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function superbowlWin(record) {
+    function isWinningYear(year) {
+        if(year.result === "W")
+        return true
+    } else {
+        return false
+    }
+}
+record.find(isWinningYear)
+const isWinningYear = record.find(isWinningYear)
+if(isWinningYear) {
+    return isWinningYearObject.isWinningYear
+}
+*/
